b7f663155ef8761ff78d404ac95b2fdd92fd8f20
SWATHIHP <swathihp2@gmail.com>
2022-04-16 19:18:37 +0530

    degreeconvert tests added

---
+++ DegreeConvertEg/DegreeConvertEg/DegreeConverTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegreeConvertEg
+{
+    [TestFixture]
+    public class DegreeConverTests
+    {
+        [Test]
+        public void ToFahrenheit_ZeroClecius_Returns32()
+        {
+            var converter = new DegreeConvert();
+            double result = converter.ToFahrenheit(0);
+
+            Assert.That(result, Is.EqualTo(32));
+        }
+        [Test]
+        public void ToCelsius_1Fahrenheit_Returns0()
+        {
+            var converter = new DegreeConvert();
+            double result = converter.ToCelsius(1);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
