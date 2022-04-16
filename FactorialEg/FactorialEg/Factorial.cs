06476af711d8468d9ca4673ba1f89dfe264b5354
SWATHIHP <swathihp2@gmail.com>
2022-04-16 18:36:40 +0530

    factorial tests added

---
+++ FactorialEg/FactorialEg/Factorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialEg
+{
+   
+    public class Factorial
+    {
+        public float GetFactorialOf(int p)
+        {
+            if (p < 2)
+                return 1;
+            return p * GetFactorialOf(p - 1);
+        }
+    }
+}
