a4d358eb66a3272d31d21b09a5fa273f819381ce
SWATHIHP <swathihp2@gmail.com>
2022-05-08 20:58:25 +0530

    removed registerusereg

--- RegisterUser/RegisterUser/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RegisterUser.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- RegisterUser/RegisterUser/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RegisterUser.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
